6b030cdfb18b680a8dd68c636153fa1fe4fac01b
Azita Taheri <96353949+azitataheri@users.noreply.github.com>
2026-02-25 10:39:07 +0330

    develop select options by usd,eur, jpy

--- src/services/cryptoApi.js
+++ src/services/cryptoApi.js
@@ -1,8 +1,8 @@
 const BASE_URL = "https://api.coingecko.com/api/v3"
 const API_KEY = "CG-8ED1FmqnQopADvixwhHb7R13"
 
-const getCoinList = (page) => {
-    return `${BASE_URL}/coins/markets?vs_currency=usd&per_page=20&page=${page}&order=market_cap_desc&x_cg_demo_api_key=${API_KEY}`
+const getCoinList = (page, currency) => {
+    return `${BASE_URL}/coins/markets?vs_currency=${currency}&per_page=20&page=${page}&order=market_cap_desc&x_cg_demo_api_key=${API_KEY}`
 }
 
 export {
