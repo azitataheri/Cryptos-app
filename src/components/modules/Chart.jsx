38625a19cf8b8bcb59d7a6b61e7f8ae052f7acb5
Azita Taheri <96353949+azitataheri@users.noreply.github.com>
2026-02-27 10:10:20 +0330

    draw chart by data and type and develop it by details

--- src/components/modules/Chart.jsx
+++ src/components/modules/Chart.jsx
@@ -1,15 +1,22 @@
 import { useState } from 'react';
 import { convertData } from '../../helper/convertData';
+
+
 import styles from '../modules/Chart.module.css'
+import ChartComponent from './ChartComponent';
 
 function Chart({chart, setChart}) {
 const[type, setType] = useState("prices")
-console.log(convertData(chart, type));
+console.log('converdata is:', convertData(chart, type));
 
   return (
     <div className={styles.container}>
         <span className={styles.cross} onClick={() => setChart(null)}>X</span>
-        <div className={styles.chart}></div>
+        <div className={styles.chart}>
+          <div className={styles.graph}>
+            <ChartComponent type={type} data={convertData(chart, type)}/>
+          </div>
+        </div>
     </div>
   )
 }
